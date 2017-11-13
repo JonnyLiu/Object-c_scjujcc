b4dfcd91e3015d44d1f65ec5b0124ca2f63acb39
guandoudou <guandoudou@guandoudous-MacBook-Air.local>
2017-11-13 20:02:32 +0800

    Initial Commit

---
+++ Lc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lc
+//
+//  Created by guandoudou on 2017/11/13.
+//  Copyright © 2017年 京. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
